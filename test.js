e351ceb2e374379c4d8f1ec419bcd707fa557f30
designeng <denissavenok@gmail.com>
2021-02-17 11:32:12 +0300

    Add test with sinon spy

--- test.js
+++ test.js
@@ -1,25 +1,39 @@
-// Import the mount() method from the test utils
-// and the component you want to test
 import { mount } from '@vue/test-utils'
-import Counter from './counter'
+import Counter from '@/counter'
 
 describe('Counter', () => {
-  // Now mount the component and you have the wrapper
-  const wrapper = mount(Counter)
+    const wrapper = mount(Counter)
 
-  it('renders the correct markup', () => {
-    expect(wrapper.html()).toContain('<span class="count">0</span>')
-  })
+    it('renders the correct markup', () => {
+        expect(wrapper.html()).toContain('<span class="count">0</span>')
+    })
 
-  // it's also easy to check for the existence of elements
-  it('has a button', () => {
-    expect(wrapper.contains('button')).toBe(true)
-  })
+    it('has a button', () => {
+        expect(wrapper.contains('button')).toBe(true)
+    })
 
-  it('button should increment the count', () => {
-    expect(wrapper.vm.count).toBe(0)
-    const button = wrapper.find('button')
-    button.trigger('click')
-    expect(wrapper.vm.count).toBe(1)
-  })
+    it('button should increment the count', () => {
+        expect(wrapper.vm.count).toBe(0)
+        const button = wrapper.find('button')
+        button.trigger('click')
+        expect(wrapper.vm.count).toBe(1)
+    })
+})
+
+import YesNoComponent from '@/src/YesNoComponent.vue'
+import sinon from 'sinon'
+
+describe('YesNoComponent', () => {
+    it('Click on yes button calls our method with argument "yes"', async () => {
+      const spy = sinon.spy()
+
+      const wrapper = mount(YesNoComponent, {
+        propsData: {
+          callMe: spy
+        }
+      })
+      await wrapper.find('button.yes').trigger('click')
+
+      expect(spy.calledWith('yes')).toBe(true)
+    })
 })
